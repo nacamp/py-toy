3e2f7d7b1885dff279b6e248cee9f4474dfc4f75
nacamp <najimmy@gmail.com>
2019-06-04 00:32:41 +0900

    move the image to be created to another directory

--- monitor/img.sh
+++ monitor/img.sh
@@ -5,8 +5,8 @@ export PYTHONPATH
 while true
 do
     echo "$@"
-    webkit2png "$@" -o new -F
-    python3 "$PYTHONPATH/monitor/img.py" "$PYTHONPATH/monitor/org.png"  "$PYTHONPATH/monitor/new-full.png"
+    webkit2png "$@" -o new -F -D "$PYTHONPATH/data"
+    python3 "$PYTHONPATH/monitor/img.py" "$PYTHONPATH/data/org.png"  "$PYTHONPATH/data/new-full.png"
     #python3 "$PYTHONPATH/monitor/img.py" "$@"
     sleep 60
 done
